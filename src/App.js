562b852f20a1c3165b2960f57198683ad29fba0c
charmee123 <92216225+charmee123@users.noreply.github.com>
2023-08-05 23:47:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Mainbody from "./components/Mainbody";
+import Template from "./components/Template";
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from "react-router-dom";
+import FormHeader from "./components/FormHeader";
+import CenteredTabs from "./components/Tabs";
+import Question_form from "./components/Question_form";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <Router>
+        <Routes>
+          <Route path="/form/:id" element={<div><FormHeader/><CenteredTabs/><Question_form/></div>} />
+          <Route path="/" element={<div><Header/><Template/><Mainbody/></div>} />
+          {/* <Route path="/" element={<Template/>} />
+          <Route path="/" element={<Mainbody/>} /> */}
+            {/* <Header />
+            <Template />
+            <Mainbody /> */}
+            </Routes>
+          </Router>
+      </React.Fragment>
     </div>
   );
 }
 
+export default App;
 export default App;
